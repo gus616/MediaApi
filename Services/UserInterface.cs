d3a7dc781f82c65ea7634a3be6b8467581226b7f
Gus Vc <guzz.vc@gmail.com>
2025-02-04 20:12:01 -0600

    user service and interface added

---
+++ Services/UserInterface.cs
@@ -0,0 +1,12 @@
+﻿using MediaApi.Models;
+
+namespace MediaApi.Services
+{
+    public interface UserInterface
+    {
+        public Task<IEnumerable<User>> GetUsers();
+        public Task<User> GetUser(int id);
+
+        public Task<User> AddUser(User user);
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using MediaApi.Models;
+
+namespace MediaApi.Services
+{
+    public class UserService : UserInterface
+    {
+        public Task<User> AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
